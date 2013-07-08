2f4bf8a7e140f395b940cd599e90d2f3f154f77a
Daniel Sanchez <danny.ne1410s@gmail.com>
2013-07-08 16:58:09 -0400

    grey bar in table, submit button

--- JUnitTable1.js
+++ JUnitTable1.js
@@ -3,12 +3,13 @@ var JUnitTable1 = (function(){
 	var exports = {};
 	var setup = function(div){
 		var table = $("<table id = 'JUnitTable1' class = 'table table-hover table-bordered'></table>");
+		var submit = $("<button id = 'submit1' class = 'btn btn-success btn-large'>Submit</button>")
 	/*Row0*/
 		var row0 = $("<tr class = 'row0'></tr>");
 		var emptyLabel = $("<td class = 'empty'></td>");
-		var findLabel1 = $("<td class = 'col1'>Find(<input id = 'xInput1' class = 'findInput' placeholder = '  x'></input>, <input id = aInput1 class = 'findInput' placeholder = '  a'></input>)</td>");
-		var findLabel2 = $("<td class = 'col2'>Find(<input id = 'xInput2' class = 'findInput' placeholder = '  x'></input>, <input id = aInput2 class = 'findInput' placeholder = '  a'></input>)</td>");
-		var findLabel3 = $("<td class = 'col3'>Find(<input id = 'xInput3' class = 'findInput' placeholder = '  x'></input>, <input id = aInput3 class = 'findInput' placeholder = '  a'></input>)</td>");
+		var findLabel1 = $("<td class = 'col1'>Find(<input id = 'xInput1' class = 'findInput1' placeholder = '  x'></input>, <input id = aInput1 class = 'findInput2' placeholder = '  a'></input>)</td>");
+		var findLabel2 = $("<td class = 'col2'>Find(<input id = 'xInput2' class = 'findInput1' placeholder = '  x'></input>, <input id = aInput2 class = 'findInput2' placeholder = '  a'></input>)</td>");
+		var findLabel3 = $("<td class = 'col3'>Find(<input id = 'xInput3' class = 'findInput1' placeholder = '  x'></input>, <input id = aInput3 class = 'findInput2' placeholder = '  a'></input>)</td>");
 
 		row0.append(emptyLabel, findLabel1, findLabel2, findLabel3);
 		table.append(row0);
@@ -22,11 +23,12 @@ var JUnitTable1 = (function(){
 				var newCol = $("<td class = 'col" + c + "'></td>");
 				/*CSS*/
 				if (r == 3){
-					var cellID
-					$(".row3 .col").css("border-bottom", "4px solid black")
+					var cellID = ".row3 .col" + c;
+					$(cellID).css("border-bottom", "4px solid black")
 				}
 				if (r == 4){
-					$(".row4").css("border-top", "4px solid black")
+					var cellID = ".row4 .col" + c;
+					$(cellID).css("border-top", "4px solid black")
 				}
 				/*Labels*/
 				if (c == 0){
@@ -50,7 +52,7 @@ var JUnitTable1 = (function(){
 			}
 			table.append(newRow);
 		}
-		$(div).append(table);
+		$(div).append(table, submit);
 	}
 	exports.setup = setup;
 	return exports;
@@ -60,4 +62,26 @@ $(document).ready(function(){
 	$(".JUnitTable1").each(function(){
 		JUnitTable1.setup(this);
 	});
+
+	/*Creates Grey bar in middle of table*/
+	for (var r = 1; r <= 6; r++){
+		var newRow = $("<tr class = 'row" + r + "'></tr>");
+		for (var c = 0; c <= 3; c++){
+			var rowClass = ".row" + r;
+			var newCol = $("<td class = 'col" + c + "'></td>");
+			if (r == 3){
+				var cellID = ".row3 .col" + c;
+				$(cellID).css("border-bottom", "3px solid grey")
+			}
+			if (r == 4){
+				var cellID = ".row4 .col" + c;
+				$(cellID).css("border-top", "3px solid grey")
+			}
+		}
+	}
+
+	/*Checks the user inputs when the submit button is pressed*/
+	$("#submit1").on("click", function(){
+		$(".findInput1")
+	})
 });
